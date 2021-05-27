5efd00f56e97b8e65d13efe28915549fff4bb51f
IvyJ <inbaejj117@gmail.com>
2021-05-27 14:17:08 +0900

    fix: response(TypeError: Converting circular structure to JSON)

--- src/controller/UrlController.ts
+++ src/controller/UrlController.ts
@@ -10,7 +10,6 @@ export class UrlController {
   private urlRepository = getRepository(Url);
 
   async shortenUrl(req: Request, res: Response, next: NextFunction) {
-    console.log("  longUrl: " + req.body.longUrl)
     const longUrl: string = req.body.longUrl;
 
     const expression = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi;
@@ -51,9 +50,9 @@ export class UrlController {
       const url = await this.urlRepository.findOne(id)
 
       if (url) {
-        return res.redirect(url.longUrl)
+        res.redirect(url.longUrl)
       } else {
-        return res.status(HttpStatus.NOT_FOUND).json("Url Not found")
+        res.status(HttpStatus.NOT_FOUND).json("Url Not found")
       }
     } catch (err) {
       console.error(err);
